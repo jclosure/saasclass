190f687dd3dc96f40a55b8ea8aba1b1869afa595
jclosure <jclosure@gmail.com>
2012-02-29 22:37:25 -0600

    solves tournament problem

--- hw1/a.rb
+++ hw1/a.rb
@@ -5,6 +5,7 @@ require 'rspec'
 class WrongNumberOfPlayersError < StandardError ; end
 class NoSuchStrategyError < StandardError ; end
 
+
 class Hw1
 
   def palindrome?(string)
@@ -19,10 +20,39 @@ class Hw1
     freqs
   end
 
+  def wins?(strategy1, strategy2)
+    (strategy1 == 'P' && strategy2 == 'R') ||
+      (strategy1 == 'S' && strategy2 == 'P') ||
+      (strategy1 == 'R' && strategy2 == 'S')
+  end
+
   def rps_game_winner(game)
     raise WrongNumberOfPlayersError unless game.length == 2
-
+    player1, player1_strategy = game[0], game[0][1].upcase
+    player2, player2_strategy = game[1], game[1][1].upcase
+    legal_strategies = ['R','P','S']
+    raise NoSuchStrategyError unless legal_strategies.include?(player1_strategy) && legal_strategies.include?(player2_strategy)
+    if (self.wins?(player1_strategy, player2_strategy))
+      player1
+    else
+      player2
+    end
   end
+
+  def rps_tournament_winner(tournament)
+
+    plays = Hash[*tournament.flatten].map { |player, strategy| [player, strategy] }
+
+    while (plays.length > 1)
+      next_plays = []
+      plays.each_slice(2) { |game| next_plays << self.rps_game_winner(game) }
+      plays = next_plays
+    end
+    
+    plays[0]
+    
+  end
+
 end
 
 
@@ -53,15 +83,60 @@ describe Hw1, "Ruby calisthenics" do
   end
 
   describe "rps game behaviors" do
-    it "should only allow 2 players" do
+    it "should only allow 2 players per game" do
       game_of_2 = [ [ "Armando", "P" ], [ "Dave", "S" ] ]
       expect { @hw1.rps_game_winner(game_of_2) }.to_not raise_error(WrongNumberOfPlayersError)
-      
+
       game_of_1 = [ [ "Armando", "P" ] ]
       expect { @hw1.rps_game_winner(game_of_1) }.to raise_error(WrongNumberOfPlayersError)
 
       game_of_3 = [ [ "Armando", "P" ], [ "Dave", "S" ], [ "Joel", "R" ] ]
       expect { @hw1.rps_game_winner(game_of_3) }.to raise_error(WrongNumberOfPlayersError)
+    end
+
+    it "should allow only legal strategies" do
+      game_with_legal_strategy = [ [ "Armando", "R" ], [ "Dave", "S" ] ]
+      expect { @hw1.rps_game_winner(game_with_legal_strategy) }.to_not raise_error(NoSuchStrategyError)
+
+      game_with_illegal_strategy = [ [ "Armando", "X" ], [ "Dave", "S" ] ]
+      expect { @hw1.rps_game_winner(game_with_illegal_strategy) }.to raise_error(NoSuchStrategyError)
+
+    end
+
+    it "should enforce that R beats S" do
+      player1 = [ "Armando", "R" ]
+      player2 = [ "Dave", "S" ]
+      game = [ player1, player2 ]
+      @hw1.rps_game_winner(game).should equal player1
+    end
+
+    it "should enforce that S beats P" do
+      player1 = [ "Armando", "P" ]
+      player2 = [ "Dave", "S" ]
+      game = [ player1, player2 ]
+      @hw1.rps_game_winner(game).should equal player2
+    end
+
+    it "should enforce that P beats R" do
+      player1 = [ "Armando", "P" ]
+      player2 = [ "Dave", "R" ]
+      game = [ player1, player2 ]
+      @hw1.rps_game_winner(game).should equal player1
+    end
+
+    it "should support a tournament at the end of which, there is a single winnder" do
+      tournament = [ [
+                       [ ["Armando", "P"], ["Dave", "S"] ],
+                       [ ["Richard", "R"], ["Michael", "S"] ],
+                     ],
+                     [
+                       [ ["Allen", "S"], ["Omer", "P"] ],
+                       [ ["David E.", "R"], ["Richard X.", "P"] ]
+                     ]
+                     ]
+
+      @hw1.rps_tournament_winner(tournament).should == ["Richard", "R"]
+
     end
   end
 
